ce04781bee4faceecd1eed3f6d411d156b343d2a
rjmarin <rjmarin@miuandes.cl>
2019-05-16 18:04:58 -0400

    fix migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_16_162048) do
+ActiveRecord::Schema.define(version: 2019_05_16_200954) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -65,7 +65,9 @@ ActiveRecord::Schema.define(version: 2019_05_16_162048) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["provider"], name: "index_users_on_provider"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid"], name: "index_users_on_uid"
   end
 
 end
