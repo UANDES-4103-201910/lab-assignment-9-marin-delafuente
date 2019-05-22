20f2d642b65468d56c6b26c802baf078da708bba
hdelafuente <hadelafuente@miuandes.c>
2019-05-22 15:54:33 -0400

    Merge

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,5 +5,18 @@ class RegistrationsController < ApplicationController
 
 	def create
 	    #complete this method
+			r = params[:registrations]
+			@user = User.create(name: r[:name],last_name: r[:last_name], email:r[:email],phone:r[:phone],
+													encrypted_password:r[:encrypted_password])
+			if @user.save
+				respond_to do |format|
+					format.html { redirect_to root_path, notice: 'User was successfully created.' }
+				end
+			else
+				flash[:alert] = "couldn't create the user"
+			end
 	end
+
+
+
 end
--- db/migrate/20180401225453_create_users.rb
+++ db/migrate/20180401225453_create_users.rb
@@ -7,6 +7,8 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.string :phone
       t.string :password
       t.string :address
+      t.string :provider
+      t.string :uid
 
       t.timestamps
     end
