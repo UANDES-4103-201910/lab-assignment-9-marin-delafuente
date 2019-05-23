734cec76de8cbee5831c6023943e5fce55d20697
rjmarin <rjmarin@miuandes.cl>
2019-05-23 14:35:58 -0400

    cancan ability working

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,11 +8,7 @@ class Ability
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
-     if user.admin?
-         can :manage, :all
-     else
-         can :read, :all
-     end
+
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
@@ -31,12 +27,18 @@ class Ability
     #
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
-    can :read, :all
+    can :read, :Event
     if user.present?
       can :create, UserTicket
       if user.admin?
         can :manage, :all
+      else
+        can :read, Place, Ticket
+        can :read, UserTicket, user_id: user.id
+        can :create, UserTicket, user_id: user.id
       end
     end
+
+
   end
 end
