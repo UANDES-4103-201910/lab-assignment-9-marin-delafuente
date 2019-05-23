2a3c858a8b840e89508a6f414dd30c4ccf47ef03
rjmarin <rjmarin@miuandes.cl>
2019-05-22 21:36:10 -0400

    lab9 ready

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,9 @@ class ApplicationController < ActionController::Base
 
   end
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
+  #def current_user
+   # @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  #end
 
   def is_user_logged_in?
 	#complete this method
